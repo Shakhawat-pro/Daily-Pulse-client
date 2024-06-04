59e2661cd0c9471bcfb7fd095ebf0f2270922190
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-06-04 18:43:51 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className="text-4xl">
+            <h1>This is Home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/TrendingArticle/TrendingArticle.jsx
@@ -0,0 +1,10 @@
+
+const TrendingArticle = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TrendingArticle;
